329d0e06d52e8f9d3297ae8890a0b273abd231dd
MinseokOh <glaxer95@gmail.com>
2020-06-29 22:31:41 +0900

    Test commit for travis setting

--- lol/champion_mastery_test.go
+++ lol/champion_mastery_test.go
@@ -1,6 +1,7 @@
 package lol_test
 
 import (
+	"fmt"
 	"go-riot/lol"
 	"testing"
 
@@ -11,8 +12,10 @@ func TestBySummoner(t *testing.T) {
 	assert := assert.New(t)
 
 	client := lol.NewClient(lol.KR, TestAPIKey, nil)
-	res, _ := client.ChampionMastery.BySummoner("aPWJgSeY9bV4Jq6DJ7lOBo3YVr9VvB_fcrdQb3NKllH8WQ")
-
+	res, err := client.ChampionMastery.BySummoner(TestSummonerEnID)
+	if err != nil {
+		fmt.Println(err)
+	}
 	assert.Equal(res[0].ChampionLevel, 7)
 }
 
@@ -20,7 +23,7 @@ func TestByChampion(t *testing.T) {
 	assert := assert.New(t)
 
 	client := lol.NewClient(lol.KR, TestAPIKey, nil)
-	res, _ := client.ChampionMastery.ByChampion("aPWJgSeY9bV4Jq6DJ7lOBo3YVr9VvB_fcrdQb3NKllH8WQ", 26)
+	res, _ := client.ChampionMastery.ByChampion(TestSummonerEnID, 26)
 
 	assert.Equal(res.ChampionLevel, 7)
 }
@@ -29,7 +32,7 @@ func TestScore(t *testing.T) {
 	assert := assert.New(t)
 
 	client := lol.NewClient(lol.KR, TestAPIKey, nil)
-	res, _ := client.ChampionMastery.Scores("aPWJgSeY9bV4Jq6DJ7lOBo3YVr9VvB_fcrdQb3NKllH8WQ")
+	res, _ := client.ChampionMastery.Scores(TestSummonerEnID)
 
 	assert.Equal(res, 541)
 }
